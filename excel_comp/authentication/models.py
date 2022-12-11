4b946cb9eef931ad42b019fb2b51ff1294575b1d
Homoakin619 <homohaykins619@gmail.com>
2022-12-11 23:27:35 +0100

    added authentication module and final build

--- excel_comp/authentication/models.py
+++ excel_comp/authentication/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-# from django.conf import settings
+from django.conf import settings
 
-# class Profile(models.Model):
-#     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-#     image = models.ImageField(blank=True,null=True)
+class Profile(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    otp = models.CharField(max_length=6)
+    is_verified = models.BooleanField(default=False)
 
-#     def __str__(self):
-#         return self.user.username
+    def __str__(self):
+        return self.user.username
--- excel_comp/authentication/views.py
+++ excel_comp/authentication/views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.views import generic
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.forms import PasswordResetForm,AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
 
+
+from authentication.models import Profile
 from authentication.forms import UserRegistrationForm
 
-class LoginRegisterView(generic.View):
+
+def validate_otp(user,otp):
+    user = get_object_or_404(User,id=user)
+    user_profile = get_object_or_404(Profile,user=user)
+    if otp == user_profile.otp:
+        user_profile.is_verified = True
+        user_profile.save()
+        return True
+    return False
+
+
+class RegisterView(generic.View):
     template_name = 'authentication/index.html'
     def get(self,request,*args,**kwargs):
         form = UserRegistrationForm()
@@ -15,16 +30,37 @@ class LoginRegisterView(generic.View):
 
     def post(self,request,*args,**kwargs):
         form = UserRegistrationForm(request.POST)
+        context = {'form':form}
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('home'))
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return HttpResponseRedirect(reverse('account_validation'))
+            else:
+                return render(request,self.template_name,context)
         else:
-            print(form.errors)
-            print('Form failed validations')
-        context = {'form':form}
-        return render(request,self.template_name,context)
+            return render(request,self.template_name,context)
+           
 
-class LoginRegisterView(generic.View):
+class AccountValidationView(generic.View):
+    template_name = 'authentication/validate_email.html'
+    def get(self,request,*args,**kwargs):
+        
+        return render(request,self.template_name)
+
+    def post(self,request,*args,**kwargs):
+        otp = request.POST.get('otp')
+        user = request.user.id
+        if validate_otp(user,otp):
+            return HttpResponseRedirect(reverse('dashboard'))
+        else:
+            return render(request,self.template_name,{'error':'incorrect codes entered try again!'})
+
+
+class LoginView(generic.View):
     template_name = 'authentication/index.html'
     def get(self,request,*args,**kwargs):
         form = AuthenticationForm()
@@ -34,17 +70,18 @@ class LoginRegisterView(generic.View):
     def post(self,request,*args,**kwargs):
         form = AuthenticationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('home'))
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return HttpResponseRedirect(reverse('email_validation'))
+            else:
+                return render(request,self.template_name,context)
         else:
-            print(form.errors)
-            print('Form failed validations')
-        context = {'form':form}
-        return render(request,self.template_name,context)
+            context = {'form':form}
+            return render(request,self.template_name,context)
 
-def home(request):
-   
-    return render(request,'authentication/home.html')
 
 class PasswordResetView(generic.View):
     template_name = 'authentication/recover_password.html'
@@ -63,69 +100,3 @@ class PasswordResetView(generic.View):
             print('Form failed validations')
         context = {'form':form}
         return render(request,self.template_name,context)
-
-# class IndexView(generic.View):
-#     template_name = 'core/login.html'
-#     def get(self,*args,**kwargs):
-     
-#         form = CustomerForm()
-#         login_form = LoginForm()
-#         context = {'form':form,'login_form':login_form,'disp':False}
-
-#         return render(self.request,self.template_name,context=context)
-    
-#     def post(self,*args,**kwargs):
-#         form = CustomerForm(self.request.POST or None)
-#         login_form = LoginForm(self.request.POST or None)
-#         context = {'form':form,'login_form':login_form,'disp':False}
-
-#         login_f = self.request.POST.get('login')
-#         signup = self.request.POST.get('signup')
-#         reset = self.request.POST.get('reset')
-#         # print(self.request)
-#         if login_f:
-#             if login_form.is_valid():
-#                 username = login_form.cleaned_data['username']
-#                 password = login_form.cleaned_data['password']
-#                 user = authenticate(self.request,username=username,password=password)
-#                 if user is not None:
-#                     login(self.request,user)
-#                     if self.request.user.is_staff:
-#                         return HttpResponseRedirect(reverse('admins'))
-#                     else:
-#                         return HttpResponseRedirect(reverse('dashboard'))
-#                 else:
-#                     print('user does not exist')
-#                     context['errors'] = login_form.errors
-#                     return render(self.request,self.template_name,context=context)
-#             else: 
-#                 context['errors'] = login_form.errors
-#                 return render(self.request,self.template_name,context=context)
-
-#         elif signup is not None:
-#             if form.is_valid():
-#                 username = form.cleaned_data['username']
-#                 password = form.cleaned_data['password1']
-#                 email = form.cleaned_data['email']
-#                 activation_key = get_activation_key()
-#                 form.save()
-#                 user = User.objects.get(username=username)
-#                 customer = Customer.objects.get(user=user)
-#                 customer.activation_key = activation_key
-#                 customer.save()
-#                 subject = 'Zeedah Account Verification'
-#                 body = f'Hi {username} \n Please click on the link below to confirm your registration \n http://{settings.DOMAIN}/activate/{activation_key}'
-#                 sender = 'zeedah@gmail.com'
-#                 with mail.get_connection() as connection:
-#                     mail.EmailMessage(
-#                         subject, body, sender, [email],
-#                         connection=connection,
-#                     ).send()
-#                 messages.success(self.request,'Kindly Check Your Email for link to activate your account')
-
-#                 return self.get(*args,**kwargs)
-#             else:
-#                 context = {'form':form,'disp':True,'errors':form.errors}
-#                 return render(self.request,self.template_name,context=context)
-
-#         return render(self.request,self.template_name,context=context)
