3529193f0dad4d75b92a73c9794bab513a8e85c8
Homoakin619 <homohaykins619@gmail.com>
2022-12-15 13:52:01 +0100

    deployment settings

--- excel_comp/excel_comp/settings.py
+++ excel_comp/excel_comp/settings.py
@@ -152,13 +152,9 @@ USE_TZ = True
 import os
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'core/static')
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
