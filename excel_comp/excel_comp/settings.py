69ce5054f6a60981ce1e90e0eb575b8d396f79a5
Homoakin619 <homohaykins619@gmail.com>
2022-12-16 05:37:51 +0100

    500 error

--- excel_comp/excel_comp/settings.py
+++ excel_comp/excel_comp/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*',
@@ -109,13 +109,13 @@ WSGI_APPLICATION = 'excel_comp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 }
-if not DEBUG:
+if  DEBUG:
     DATABASE_URL = env('DATABASE_URL')
 
     DATABASES = {
